9bd7023a2081810c3908f71a88db74faec83730b
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-11-03 15:49:30 +0530

    start page

--- app/page.tsx
+++ app/page.tsx
@@ -19,18 +19,19 @@ const Hero = () => {
   return (
     <div className='section bg-[#004733]'>
       <div className='m-auto max-w-screen-2xl overflow-hidden px-6 md:px-10 lg:px-14'>
-          <h1 className='font-semibold leading-none w-auto tracking-tighter z-10 text-center text-[#1EE07F] md:relative text-[50px] lg:text-[120px] top-0 md:top-32 '>
+          <h1 className='font-semibold leading-none w-auto tracking-tighter z-10 text-center text-[#1EE07F] md:relative text-[50px] lg:text-[120px] top-0 md:top-32 mt-10 md:mt-0 '>
             Mortgages<br/>made simple
           </h1>
         <div className='relative flex flex-col items-center justify-center md:flex-row'>
-          <div className="w-80 md:w-auto">
+          <div className="w-80 md:w-auto mt-7 md:mt-0">
             <a className="inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-bold leading-normal disabled:pointer-events-none disabled:opacity-50 transition-all ease-in-out duration-200 bg-[#1EE07F] text-black hover:bg-[#004733] hover:text-white h-16 px-12 py-5 mt-lg w-full md:mt-none md:w-fit" 
-                href="/start">Start my approval</a><div className="mt-3 flex items-center justify-center gap-1 text-sm text-accentSecondary"><svg width="20" height="20" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7.5 6H8.5V10.5H7.5V6ZM6.5 1.5H9.5V2.5H6.5V1.5Z" className="fill-accentSecondary"></path>
-                  <path className="fill-accentSecondary" d="M13.9987 5.00055L13.2887 4.29555L12.1637 5.42055C11.2429 4.35706 9.94762 3.68926 8.54717 3.5559C7.14671 3.42254 5.74872 3.83386 4.64365 4.70442C3.53857 5.57497 2.81139 6.83782 2.61318 8.23057C2.41497 9.62333 2.76098 11.0389 3.57931 12.1832C4.39765 13.3275 5.62539 14.1125 7.00745 14.3751C8.3895 14.6377 9.81961 14.3578 11.0006 13.5934C12.1817 12.8291 13.0228 11.6391 13.3493 10.2707C13.6758 8.90233 13.4625 7.46077 12.7537 6.24554L13.9987 5.00055ZM7.99875 13.5005C7.10873 13.5005 6.2387 13.2366 5.49868 12.7422C4.75866 12.2477 4.18189 11.5449 3.84129 10.7226C3.5007 9.90035 3.41158 8.99555 3.58522 8.12264C3.75885 7.24972 4.18743 6.4479 4.81677 5.81856C5.44611 5.18923 6.24793 4.76064 7.12084 4.58701C7.99376 4.41338 8.89856 4.50249 9.72082 4.84309C10.5431 5.18368 11.2459 5.76046 11.7404 6.50048C12.2348 7.2405 12.4987 8.11053 12.4987 9.00055C12.4987 10.194 12.0246 11.3386 11.1807 12.1825C10.3368 13.0264 9.19222 13.5005 7.99875 13.5005Z"></path></svg>
-                  <span className="mr-1 font-medium">3 min</span><span className='font-medium'>| No credit impact</span>
+                href="/start">Start my approval</a>
+                
+              <div className="mt-3 flex items-center justify-center gap-1 text-sm text-accentSecondary">
+              <svg width="20" height="20" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.5 6H8.5V10.5H7.5V6ZM6.5 1.5H9.5V2.5H6.5V1.5Z" className="fill-accentSecondary"></path><path className="fill-accentSecondary" d="M13.9987 5.00055L13.2887 4.29555L12.1637 5.42055C11.2429 4.35706 9.94762 3.68926 8.54717 3.5559C7.14671 3.42254 5.74872 3.83386 4.64365 4.70442C3.53857 5.57497 2.81139 6.83782 2.61318 8.23057C2.41497 9.62333 2.76098 11.0389 3.57931 12.1832C4.39765 13.3275 5.62539 14.1125 7.00745 14.3751C8.3895 14.6377 9.81961 14.3578 11.0006 13.5934C12.1817 12.8291 13.0228 11.6391 13.3493 10.2707C13.6758 8.90233 13.4625 7.46077 12.7537 6.24554L13.9987 5.00055ZM7.99875 13.5005C7.10873 13.5005 6.2387 13.2366 5.49868 12.7422C4.75866 12.2477 4.18189 11.5449 3.84129 10.7226C3.5007 9.90035 3.41158 8.99555 3.58522 8.12264C3.75885 7.24972 4.18743 6.4479 4.81677 5.81856C5.44611 5.18923 6.24793 4.76064 7.12084 4.58701C7.99376 4.41338 8.89856 4.50249 9.72082 4.84309C10.5431 5.18368 11.2459 5.76046 11.7404 6.50048C12.2348 7.2405 12.4987 8.11053 12.4987 9.00055C12.4987 10.194 12.0246 11.3386 11.1807 12.1825C10.3368 13.0264 9.19222 13.5005 7.99875 13.5005Z"></path></svg>
+                <span className="mr-1 font-medium">3 min</span><span className='font-medium'>| No credit impact</span>
+              </div>
           </div>
-        </div>
           <div className="mt-5 md:mb-[-36px] md:mt-0">
             <Image alt="Better Mortgage" className='bg-transparent pointer-events-none z-0' rel="preload" fetchPriority="high" width="510" height="810" decoding="async" data-nimg="1" src="/hero.webp"/>
           </div>
--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,8 +1,95 @@
+import Footer from '@/components/Footer'
 import React from 'react'
 
 const start = () => {
   return (
-    <div>start</div>
+    <div className='bg-[#fffdfa] montserrat'>
+        <Nav/>
+        <Main/>
+        <Footer/>
+    </div>
+  )
+}
+
+const Nav = () => {
+  return (
+    <div>nav</div>
+  )
+}
+
+const Main = () => {
+  return (
+    <div className="flex flex-col justify-center text-center mx-auto px-20 py-8 text-black w-1/2">
+        <h1 className="text-4xl font-bold mb-2">Hi, I'm Betsy!</h1>
+        <h2 className="text-4xl font-bold">What can I help you with?</h2>
+        <div className='w-full gap-5 flex flex-col justify-center pt-8 '>
+            <button className='px-10 py-3 mx-6  rounded-lg bg-transparent font-bold border-[1px] border-gray-400 flex'>
+                <div>
+                <svg fill="transparent" height="36" viewBox="0 0 36 36" width="36" xmlns="http://www.w3.org/2000/svg"><title id="start-purchase-icon">Start Purchase</title><path d="M35.579 13.748 18.703.247a1.124 1.124 0 0 0-1.406 0L.42 13.747a1.125 1.125 0 0 0 .698 2.003h2.25v19.126A1.126 1.126 0 0 0 4.494 36h27.001a1.125 1.125 0 0 0 1.125-1.125V15.75h2.25a1.125 1.125 0 0 0 .709-2.002Zm-4.084-.248a1.125 1.125 0 0 0-1.125 1.125V33.75H5.619V14.625A1.126 1.126 0 0 0 4.494 13.5h-.169l13.67-10.936L31.662 13.5h-.168Z" fill="#017848"></path><path d="M16.8 13v2.374h-1.2a3.607 3.607 0 0 0-3.118 1.78 3.527 3.527 0 0 0 0 3.56 3.607 3.607 0 0 0 3.118 1.78h4.8c.429 0 .825.227 1.04.594.214.367.214.82 0 1.187-.215.367-.611.593-1.04.593h-4.8c-.318 0-.624-.125-.849-.347a1.18 1.18 0 0 1-.351-.84H12c0 .945.38 1.85 1.054 2.518a3.622 3.622 0 0 0 2.546 1.043h1.2v2.373h2.4v-2.373h1.2a3.607 3.607 0 0 0 3.118-1.78 3.526 3.526 0 0 0 0-3.56A3.607 3.607 0 0 0 20.4 20.12h-4.8c-.429 0-.825-.226-1.04-.593a1.176 1.176 0 0 1 0-1.187c.215-.367.611-.594 1.04-.594h4.8c.318 0 .624.125.849.348.225.222.351.524.351.84H24c0-.945-.38-1.85-1.054-2.519a3.621 3.621 0 0 0-2.546-1.042h-1.2V13h-2.4Z" fill="#017848"></path></svg>
+                </div>
+                <span className="pl-10 font-semibold text-xl items-center">Buying a home</span>
+            </button>
+            <button className='px-10 py-3 mx-6  rounded-lg bg-transparent font-bold border-[1px] border-gray-400 flex'>
+                <div>
+                <svg fill="transparent" height="36" viewBox="0 0 36 36" width="36" xmlns="http://www.w3.org/2000/svg"><title id="start-refinance-icon">Start Refinance</title><path d="M35.579 13.748 18.703.247a1.124 1.124 0 0 0-1.406 0L.42 13.747a1.125 1.125 0 0 0 .698 2.003h2.25v19.126A1.126 1.126 0 0 0 4.494 36h27.001a1.125 1.125 0 0 0 1.125-1.125V15.75h2.25a1.125 1.125 0 0 0 .709-2.002Zm-4.084-.248a1.125 1.125 0 0 0-1.125 1.125V33.75H5.619V14.625A1.126 1.126 0 0 0 4.494 13.5h-.169l13.67-10.936L31.662 13.5h-.168Z" fill="#017848"></path><path d="M17.6 15v1.768h-.9c-.965 0-1.856.505-2.338 1.326a2.61 2.61 0 0 0 0 2.651 2.709 2.709 0 0 0 2.338 1.326h3.6c.322 0 .619.169.78.442a.87.87 0 0 1 0 .884.903.903 0 0 1-.78.442h-3.6a.908.908 0 0 1-.636-.259.876.876 0 0 1-.264-.625H14c0 .704.284 1.378.79 1.876a2.725 2.725 0 0 0 1.91.776h.9v1.768h1.8v-1.768h.9c.965 0 1.856-.505 2.338-1.326a2.61 2.61 0 0 0 0-2.651 2.709 2.709 0 0 0-2.338-1.326h-3.6a.903.903 0 0 1-.78-.442.87.87 0 0 1 0-.884.903.903 0 0 1 .78-.442h3.6c.239 0 .468.093.636.259.17.165.264.39.264.625H23c0-.704-.285-1.378-.79-1.876a2.725 2.725 0 0 0-1.91-.776h-.9V15h-1.8Z" fill="#017848"></path><path d="M18.456 10C12.5 10 7.67 14.83 7.67 20.785H6l2.869 4.782 2.865-4.782h-1.667a8.383 8.383 0 0 1 8.389-8.388 8.383 8.383 0 0 1 8.389 8.388 8.383 8.383 0 0 1-8.39 8.389 8.32 8.32 0 0 1-5.919-2.469l-1.702 1.702a10.73 10.73 0 0 0 7.622 3.164c5.956 0 10.785-4.83 10.785-10.786C29.241 14.83 24.411 10 18.456 10Z" fill="#017848"></path></svg>
+                </div>
+                <span className="pl-10 font-semibold text-xl items-center">Refinance my mortgage</span>
+            </button>
+            <button className='px-10 py-3 mx-6  rounded-lg bg-transparent font-bold border-[1px] border-gray-400 flex'>
+                <div>
+                <svg fill="transparent" height="36" viewBox="0 0 36 36" width="36" xmlns="http://www.w3.org/2000/svg"><title id="start-cashout-refinance-icon">Start Cashout Refinance</title><path d="M2.25 18a15.75 15.75 0 0 1 31.5 0H36a18 18 0 1 0-18 18v-2.25A15.75 15.75 0 0 1 2.25 18Z" fill="#017848"></path><path d="M23.143 20.204c0-.877-.352-1.718-.98-2.338a3.363 3.363 0 0 0-2.363-.968h-4.457c-.398 0-.766-.21-.965-.551a1.092 1.092 0 0 1 0-1.102c.199-.341.567-.551.965-.551H19.8c.296 0 .579.116.788.323.209.206.326.487.326.779h2.229c0-.877-.352-1.718-.98-2.338a3.363 3.363 0 0 0-2.363-.968h-1.114v-2.204h-2.229v2.204h-1.114a3.35 3.35 0 0 0-2.895 1.653 3.275 3.275 0 0 0 0 3.306 3.35 3.35 0 0 0 2.895 1.653H19.8c.398 0 .766.21.965.551.199.341.199.761 0 1.102-.199.341-.567.551-.965.551h-4.457a1.12 1.12 0 0 1-.788-.323 1.096 1.096 0 0 1-.326-.779H12c0 .877.352 1.718.979 2.338.627.62 1.477.968 2.364.968h1.114v2.204h2.229V23.51H19.8c.887 0 1.737-.348 2.364-.968.627-.62.979-1.461.979-2.338ZM29.852 19.97a1.13 1.13 0 0 0-1.429 0l-5.714 4.653a1.17 1.17 0 0 0-.423.907v9.307c0 .308.12.604.334.822.215.218.506.341.809.341h11.428c.303 0 .594-.123.808-.34.215-.219.335-.515.335-.823V25.53a1.171 1.171 0 0 0-.434-.907l-5.714-4.654Zm3.862 13.703H31.43v-3.49c0-.308-.12-.604-.335-.822a1.133 1.133 0 0 0-.808-.34H28c-.303 0-.594.122-.808.34a1.174 1.174 0 0 0-.335.822v3.49h-2.285V26.09l4.571-3.723 4.571 3.723v7.584Z" fill="#017848"></path></svg>
+                </div>
+                <span className="pl-10 font-semibold text-xl items-center">Get cash from my home</span>
+            </button>
+        </div>
+        <div className='flex'>
+            <div className='flex flex-col justify-center items-center px-2 py-8'>
+                <span className='font-semibold text-3xl py-4'>
+                $100B
+                </span>
+                <span className='text-sm text-gray-500'>
+                home loans funded entirely online
+                </span>
+            </div>
+            <div className='flex flex-col justify-center items-center px-2 py-6'>
+                <span className='font-semibold text-3xl py-4'>
+                    400K
+                </span>
+                <span className='text-sm text-gray-500'>
+                    Customers who chose a Better Mortgage
+                </span>
+            </div>
+        </div>
+        <div className='flex flex-col py-6 gap-4 font-medium bg-emerald-50 rounded-lg'>
+            <p>After a few questions, you'll unlock:</p>
+            <div className='flex flex-col justify-center gap-3'>
+                <div className='flex justify-center'>
+                    <div>
+                        <svg fill="none" height="22" width="22" xmlns="http://www.w3.org/2000/svg"><title id="percentage-outlined-icon">Percentage Outlined</title><path clipRule="evenodd" d="M10 .833C4.94.833.833 4.94.833 10S4.94 19.166 10 19.166 19.167 15.06 19.167 10C19.167 4.94 15.06.833 10 .833Zm0 16.5c-4.042 0-7.333-3.29-7.333-7.333S5.957 2.666 10 2.666 17.333 5.957 17.333 10c0 4.042-3.29 7.333-7.333 7.333ZM7.458 15H5.5l7.057-10h1.958L7.458 15ZM7 9.476a1.71 1.71 0 0 0 1.716-1.738C8.716 6.748 7.946 6 7 6c-.968 0-1.738.748-1.738 1.738S6.032 9.476 7 9.476Zm6 4.5a1.71 1.71 0 0 0 1.716-1.738c0-.99-.77-1.738-1.716-1.738-.968 0-1.738.748-1.738 1.738s.77 1.738 1.738 1.738Z" fill="#017848" fillRule="evenodd"></path></svg>
+                    </div>
+                    <p>
+                        Custom mortgage rates
+                    </p>
+                </div>
+                <div className='flex justify-center'>
+                    <div>
+                     <svg className="w-5 h-5 fill-green-800" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="MonetizationOnOutlinedIcon"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m.31-8.86c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42"></path></svg>
+                    </div>
+                    <p>
+                        Custom mortgage rates
+                    </p>
+                </div>
+                <div className='flex justify-center'>
+                    <div>
+                    <svg className="w-5 h-5 fill-green-800" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PhoneIphoneOutlinedIcon"><path d="M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1m-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5m4.5-4H7V4h9z"></path></svg>
+                    </div>
+                    <p>
+                        Custom mortgage rates
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
